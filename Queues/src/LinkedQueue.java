862804de5bf4621fccc4edf055f435a992ce2d34
samuelharriman23 <43409282+samuelharriman23@users.noreply.github.com>
2018-11-02 11:51:32 +0000

    Final implementation of Dynamic Queues in java
    Final GUI and implementation of Dynamic Queues in java

---
+++ Queues/src/LinkedQueue.java
@@ -0,0 +1,92 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author K00200540
+ */
+public class LinkedQueue {
+        private Node head, tail;
+	private int qSize;
+        
+    public LinkedQueue()
+	{
+		head = tail = null;
+		qSize = 0;
+	}
+
+
+	// accessor methods
+    	public int size() 
+	{
+		return qSize;
+	}
+         
+         public Node getTail() 
+	{
+                
+		return tail;
+	}
+
+	public boolean isEmpty()
+	{
+		return (head == null);
+	}
+        public Object front() 
+    		throws QueueEmptyException
+	{
+		if (isEmpty())
+		throw new QueueEmptyException ("Queue is Empty");
+		else
+			return head.getElement();
+	}
+        
+        public Object back() 
+    		throws QueueEmptyException
+	{
+		if (isEmpty())
+		throw new QueueEmptyException ("Queue is Empty");
+		else
+			return tail.getElement();
+	}
+        
+        public class QueueEmptyException extends RuntimeException
+{
+	public QueueEmptyException(String err)
+	{
+		super(err);
+	}
+}
+        public Object serve( ) throws QueueEmptyException 
+	// Remove the first object from the queue
+	{
+		Object obj;
+		if (qSize == 0)
+		throw new QueueEmptyException ("Queue is empty.");
+		else {
+			obj = head.getElement();
+			head = head.getNext();
+			qSize--;
+		}
+		if (qSize == 0)
+			tail = null; // the queue is now empty
+		return obj;	
+	  }
+
+        public void append (Object obj) // insert an element 
+	{
+		Node node = new Node( );
+		node.setElement(obj);
+		node.setNext(null);     	// node will be new tail node
+		if (qSize == 0)
+			head = node;      	 // empty queue
+		else
+			tail.setNext(node); // add node at the tail of the list
+		
+		tail = node;    // update the reference to the tail node
+		qSize++;
+	}
+}
---
+++ Queues/src/Node.java
@@ -0,0 +1,45 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author K00200540
+ */
+public class Node {
+    private Object element;// element stored here
+    private Node next;//reference stored here to next in list
+    
+    // constructors
+    public Node() 
+    {
+        this(null,null);
+    }
+
+    public Node(Object e, Node n) 
+    {
+        element = e;
+        next = n;
+    }
+    public void setElement(Object newElem)
+    {
+    element = newElem;
+    }
+
+    public void setNext(Node newNext) 
+    {
+        next = newNext;
+    }
+
+    public Object getElement() 
+    {
+        return element;
+    }
+
+    public Node getNext() 
+    {
+        return next;
+    }
+}
